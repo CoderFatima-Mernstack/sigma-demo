f816c24b5f7a0acf7639e2f346fa4791135bbc3f
CoderFatima-Mernstack <gorijuned00@gmail.com>
2026-09-19 15:46:01 +0530

    js new project

---
+++ index.js
@@ -0,0 +1,28 @@
+const currencyFirstE1=document.getElementById("currency-first");
+const worthFirstE1=document.getElementById("worth-first");
+
+const currencySecondE1=document.getElementById("currency-second");
+const worthSecondE1=document.getElementById("worth-second");
+
+const exchangeRateE1= document.getElementById("exchange-rate");
+
+updateRate()
+
+function updateRate(){
+fetch(`https://v6.exchangerate-api.com/v6/babecfe8338a5b60e28aab81/latest//${currencyFirstE1.value}`)
+.then((res)=>res.json())
+.then((data)=> {
+    const rate= data.conversion_rates[currencySecondE1.value];
+    console.log(rate);
+    exchangeRateE1.innerText =`1 ${currencyFirstE1.value} =${ rate+" "+currencySecondE1.value}`;
+
+    worthSecondE1.value =(worthFirstE1.value*rate).toFixed(2)
+});
+
+}
+currencyFirstE1.addEventListener("change",updateRate);
+
+currencySecondE1.addEventListener("change",updateRate);
+
+worthFirstE1.addEventListener("input",updateRate);
+//worthSecondE1.addEventListener("input",updateRate);
